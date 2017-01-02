fe684892d7c750e0e53061c3a0fe6b56251d9f6f
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2017-01-02 18:08:18 +0900

    2017_1_2 step3 finish

--- src/main/jp/ac/uryukyu/ie/e165706/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e165706/LivingThing.java
@@ -1,7 +1,7 @@
 package jp.ac.uryukyu.ie.e165706;
 
 
-public  class LivingThing {
+public  abstract class LivingThing {
     private String name;
     private int hitPoint;
     private int attack;
@@ -14,29 +14,36 @@ public  class LivingThing {
         dead = false;
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
-    public  void  setDead(boolean dead){this.dead = dead;}
     public boolean isDead(){return dead;}
+    public  void  setDead(boolean dead){this.dead = dead;}
 
 
-    public String getName(){return name;}
+
+    public  String getName(){return name;}
     public void setName(String name){this.name = name;}
 
-    public  void setHitPoint(int hitPoint){this.hitPoint = hitPoint;}
+
     public int getHitPoint() {return hitPoint;}
+    public  void setHitPoint(int hitPoint){this.hitPoint = hitPoint;}
+
+
 
     public void attack(LivingThing opponent){
         if (dead == false) {
-            int damage = (int) (Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            int damage = (int) (Math.random()  * attack);
+            System.out.printf("%sの攻撃！\n", name);
+            damage = attack_option(damage,opponent);
             opponent.wounded(damage);
         }
         else{
             dead = true;
         }
     }
+    public abstract int attack_option(int damage,LivingThing opponent);
 
     public void wounded(int damage){
         hitPoint -= damage;
+        //System.out.println("残り体力は" + getHitPoint());
         if( hitPoint <= 0 ) {
             dead = true;
             System.out.printf("%sは倒れた。\n", name);
